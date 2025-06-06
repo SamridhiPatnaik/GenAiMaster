382a85bb7078b5b19ffa10a7c4a4bfea565f536f
SamridhiPatnaik <patnaiksamridhi6@gmail.com>
2025-06-06 22:27:50 +0530

    Q11 is completed

---
+++ src/Question11.kt
@@ -0,0 +1,9 @@
+fun main(){
+    var strength = 0
+    println("Start making your coffee")
+    while (strength < 80){
+        strength +=10
+        println("Brewing.. Strength is $strength")
+    }
+    println("Coffee is ready!Strength reached $strength")
+}

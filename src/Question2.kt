2dc2d8f87b30720d54e81a88a28a47a7e798e3a4
SamridhiPatnaik <patnaiksamridhi6@gmail.com>
2025-06-06 20:25:28 +0530

    Question 2 is done

---
+++ src/Question2.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val favoriteCoffee1 = "ESPRESSO"
+    val favoriteCoffee2 = "AMERICANO"
+    println("Customer's favorite coffee: ${favoriteCoffee1.lowercase()}")
+    println("Customer's second favorite coffee: ${favoriteCoffee2.lowercase()}")
+}

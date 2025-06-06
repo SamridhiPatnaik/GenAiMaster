be78621cef1139a46366d999abe6f721f5009288
SamridhiPatnaik <patnaiksamridhi6@gmail.com>
2025-06-06 21:25:05 +0530

    Question 6 is completed

---
+++ src/Question6.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val day = 3
+    when (day) {
+        1 -> println("It is Monday!")
+        2 -> println("It is Tuesday")
+        3 -> println("Wednesday")
+        4 -> println("Thursday")
+        5 -> println("Friday")
+        6 -> println("Saturday")
+        7 -> println("It is Sunday!")
+    }
+}

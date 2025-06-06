e1d8492bda366d2ce546655ee2932ef15be56bdc
SamridhiPatnaik <patnaiksamridhi6@gmail.com>
2025-06-06 21:09:03 +0530

    Question 5 is completed

---
+++ src/Question5.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val orderNum = 5
+    if (orderNum % 2 == 0) {
+        println("Order no. $orderNum is even Have a Fun Day")
+    }
+    else {
+        println("Order no. $orderNum is odd Have a Lucky Day")
+    }
+}

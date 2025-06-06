1481df633bc080b950268e25a55e0aaa8d576e28
SamridhiPatnaik <patnaiksamridhi6@gmail.com>
2025-06-06 22:11:03 +0530

    Q9 is completed

---
+++ src/Question9.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val customerAge = 22
+    if (customerAge >= 60) {
+        println("Age $customerAge: Eligible for a senior discount")
+    }
+    else{
+        println("Age $customerAge is not for discount")
+    }
+}

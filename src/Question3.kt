d9ce32a66630f66cb94ae12dd4bd8e54a822bb50
SamridhiPatnaik <patnaiksamridhi6@gmail.com>
2025-06-06 20:50:28 +0530

    Question 3 is done

---
+++ src/Question3.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val Americano_Price = 500
+    val Espresso_Price = 600
+    println("Hot Americano costs Rs $Americano_Price and Cold Espresso costs Rs $Espresso_Price An Exclusive offer should try it")
+}

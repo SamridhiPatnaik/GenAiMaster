b53917904b8ec83235b76b0028f55abfa78e70f9
SamridhiPatnaik <patnaiksamridhi6@gmail.com>
2025-06-11 00:08:46 +0530

    Q16 is completed

---
+++ src/question16.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val orderName = "Latte"
+
+    if (orderName.length >= 5) {
+        println("Order name '$orderName' is valid.")
+    } else {
+        println("Order name '$orderName' is too short.")
+    }
+}
